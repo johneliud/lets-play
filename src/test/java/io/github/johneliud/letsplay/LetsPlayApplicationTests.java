2f93211d8f03f6ec1e25b4099e0a7a614cebacc5
John <johneliud4@gmail.com>
2026-01-08 10:37:07 +0300

    Initial commit.

---
+++ src/test/java/io/github/johneliud/letsplay/LetsPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.johneliud.letsplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
