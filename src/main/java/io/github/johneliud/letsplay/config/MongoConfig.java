0f31c2cd73691b9b32977a27b794ee5c32c3b01a
John <johneliud4@gmail.com>
2026-01-10 19:24:45 +0300

    feat: add MongoConfig to enable auditing support
    This configuration class enables MongoDB auditing annotations such as @CreatedDate and @LastModifiedDate. This allows automatic timestamp population for entity fields, ensuring consistent tracking of creation and modification times across the application's data models.

---
+++ src/main/java/io/github/johneliud/letsplay/config/MongoConfig.java
@@ -0,0 +1,9 @@
+package io.github.johneliud.letsplay.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+}
