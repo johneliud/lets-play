403dc1f875340fdb0d526e66d876278fb2810aad
John <johneliud4@gmail.com>
2026-01-13 09:48:57 +0300

    chore: add constructors to ProductResponse DTO
    This adds both no-arguments and all-arguments constructors to the ProductResponse class to enhance serialization support and testing flexibility. The change maintains the existing data structure while providing complete constructor coverage for product response handling.

--- src/main/java/io/github/johneliud/letsplay/dto/product/ProductResponse.java
+++ src/main/java/io/github/johneliud/letsplay/dto/product/ProductResponse.java
@@ -1,9 +1,13 @@
 package io.github.johneliud.letsplay.dto.product;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductResponse {
     
     private String id;
